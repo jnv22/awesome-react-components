53d65570e4fff6a46ddbfb8e0d441ddb1dfcaeff
Jordan Vartanian <jnv22@cornell.edu>
2020-08-17 23:29:18 -0400

    remove unnecessary files

--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Button } from "./index";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 
 describe("Button tests", () => {
   it("renders content", () => {
     const label = "hello button";
-    const { getByText } = render(<Button label={label} />);
-    expect(getByText(label)).toBeInTheDocument();
+    render(<Button label={label} />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 
   it("calls onclick prop", () => {
     const onClick = jest.fn();
     const label = "Submit";
-    const { getByText } = render(<Button label={label} onClick={onClick} />);
-    fireEvent.click(
-      getByText(label),
-      new MouseEvent("click", {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    render(<Button label={label} onClick={onClick} />);
+    userEvent.click(screen.getByText(label));
     expect(onClick).toHaveBeenCalled();
   });
 });
